f5f9f04c4fcce25bba461781295d8f1f4a312847
ijimnun907 <ijimnun907@g.educaand.es>
2025-03-10 19:34:36 +0100

    Restricciones para ver crear partidos y borrarlos

--- src/Entity/Partido.php
+++ src/Entity/Partido.php
@@ -3,8 +3,10 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
 use App\Repository\PartidoRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
@@ -14,7 +16,8 @@ use Doctrine\ORM\Mapping as ORM;
     operations: [
         new GetCollection(security: 'is_granted("ROLE_INSCRIPTOR")'),
         new Get(security: 'is_granted("PARTIDO_VIEW", object)'),
-
+        new Post(security: 'is_granted("ROLE_INSCRIPTOR")'),
+        new Delete(security: 'is_granted("ROLE_INSCRIPTOR")')
     ]
 )]
 class Partido
